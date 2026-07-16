cf0b37bc19ab17c7c6d75fb08f782e9d6fa3b8f4
DeerSpotter <m.teleguz1@gmail.com>
2026-07-16 11:52:52 -0400

    Add MDZipViewer application entry point

---
+++ MDZipViewer/Program.cs
@@ -0,0 +1,11 @@
+namespace MDZipViewer;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm(args.FirstOrDefault()));
+    }
+}
